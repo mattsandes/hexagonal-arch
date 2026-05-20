e653ad485ddf0bd9224f1e6b1f1244466222c20e
Mateus Sandes <mateussandes@hotmail.com.br>
2026-05-20 01:01:18 -0300

    Corrigido a injecção de dependencia do FindCustomerById

--- src/main/java/br/com/sandes/application/core/usecase/DeleteCustomerByIdUseCase.java
+++ src/main/java/br/com/sandes/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -2,16 +2,17 @@ package br.com.sandes.application.core.usecase;
 
 import br.com.sandes.application.ports.in.DeleteCustomerByIdInputPort;
 import br.com.sandes.application.ports.in.FindCustomerByIdInputPort;
+import br.com.sandes.application.ports.out.DeleteCustomerByIdOutputPort;
 
 public class DeleteCustomerByIdUseCase implements DeleteCustomerByIdInputPort {
 
-    private final DeleteCustomerByIdUseCase deleteCustomerByIdUseCase;
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
     private final FindCustomerByIdInputPort findCustomerByIdInputPort;
 
     public DeleteCustomerByIdUseCase(
-            DeleteCustomerByIdUseCase deleteCustomerByIdUseCase,
+            DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort,
             FindCustomerByIdInputPort findCustomerByIdInputPort) {
-        this.deleteCustomerByIdUseCase = deleteCustomerByIdUseCase;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
         this.findCustomerByIdInputPort = findCustomerByIdInputPort;
     }
 
@@ -19,6 +20,6 @@ public class DeleteCustomerByIdUseCase implements DeleteCustomerByIdInputPort {
     public void delete(String id) {
         var customer = findCustomerByIdInputPort.find(id);
 
-        deleteCustomerByIdUseCase.delete(customer.getId());
+        deleteCustomerByIdOutputPort.deleteById(customer.getId());
     }
 }
